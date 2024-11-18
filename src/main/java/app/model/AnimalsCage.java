59f37c9570cdf077902750733af55d267627f49a
ilyamikhailov <79221803601me@gmail.com>
2024-11-18 21:35:36 +0300

    Изменения

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,29 +7,26 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-  private Animal animal;
-  private Animal animal2;
-  private Timer timer;
-
+    private Animal animal;
+    private Animal animal2;
+    private Timer timer;
 
 
     @Autowired
-    public AnimalsCage(@Qualifier("cat") Animal animal1, @Qualifier("dog") Animal animal2, Timer timer){
+    public AnimalsCage(@Qualifier("cat") Animal animal1, @Qualifier("dog") Animal animal2, Timer timer) {
         this.animal = animal1;
         this.animal2 = animal2;
         this.timer = timer;
     }
-    
+
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString()+ " " + animal2.toString());
+        System.out.println(animal.toString() + " " + animal2.toString());
         System.out.println("At:");
         System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
-
     public Timer getTimer() {
         return timer;
     }
 }
-// У МЕНЯ ЗДЕСЬ РАЗНИЦА В ДЕСЯТИТЫСЯЧНЫХ ДОЛЯХ СЕКУНДЫ , ОДНОГО ВРЕМЕНИ ЖЕ НЕ МОЖЕТ БЫТЬ ПРИ ВЫПОЛНЕНИИ ЦИКЛА
