8f4afba8651d911f78fd8e18292cbd5d0f72f9b0
ilyamikhailov <79221803601me@gmail.com>
2024-11-19 14:47:47 +0300

    Изменения

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,25 +7,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-    private Animal animal;
-    private Animal animal2;
-    private Timer timer;
+  private Animal animal;
+  private Timer timer;
+
 
 
     @Autowired
-    public AnimalsCage(@Qualifier("cat") Animal animal1, @Qualifier("dog") Animal animal2, Timer timer) {
+    public AnimalsCage(@Qualifier("dog") Animal animal1, Timer timer){
         this.animal = animal1;
-        this.animal2 = animal2;
         this.timer = timer;
     }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString() + " " + animal2.toString());
+        System.out.println(animal.toString());
         System.out.println("At:");
         System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
+
     public Timer getTimer() {
         return timer;
     }
